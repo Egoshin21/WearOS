11036792e227b9d816281df986724a0c65159370
Anton Egoshin <antonegoshin2020@yandex.ru>
2021-12-06 16:37:23 +0300

    a

--- app/src/main/java/com/example/we/Chat.kt
+++ app/src/main/java/com/example/we/Chat.kt
@@ -1,7 +1,6 @@
 package com.example.we
 
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.view.View
